0b4c4eaba98066c09adc78d2e7caae20e2724f55
Miroslav Mali <mixlaproduction@gmail.com>
2021-10-10 18:03:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,39 @@ from data import resources as res
 
 
 class CoffeeMachine:
-    def __init__(self):
-        self.user_choice = input("What would you like to order? Espresso, latte or cappuccino? ")
 
     def run(self):
-        # Ask a user for the drink name and store it
         answer = self._user_prompt().lower()
-        # Check if the machine has enough resources for the wanted drink (return True)
-        if self._has_enough_resources(answer):
-            print("enough resources")
+        processed_answer = self._process_user_answer(answer)
+
+        if processed_answer == "drink":
+            if self._has_enough_resources(answer):
+                self._ask_for_payment(answer)
+
+
+            else:
+                print("Not enough resources.")
+                print("The maintenance team has been informed the problem. We are sorry about this.")
+        else:
+            print("go to action")
+
+    def _ask_for_payment(self, drink_type):
+        print(f"The {drink_type} price is {MENU[drink_type]['cost']}")
+        quarters = input("How many quarters you are paying with? ")
+
+
+    def _process_coins(self):
+        pass
+
+    @staticmethod
+    def _process_user_answer(user_answer):
+        if user_answer in ["espresso", "latte", "cappuccino"]:
+            return "drink"
+        else:
+            return "action"
 
     def _user_prompt(self):
+        self.user_choice = input("What would you like to order? Espresso, latte or cappuccino? ")
         while self.user_choice.lower() not in ["espresso", "latte", "cappuccino", "report", "off"]:
             print("Invalid answer. Please type the drink name exactly as is.")
             self.user_choice = input("What would you like to order? Espresso, latte or cappuccino? ")
@@ -25,9 +47,13 @@ class CoffeeMachine:
         if wanted_drink == "espresso":
             if res["water"] > ingredients["water"] and res["coffee"] > ingredients["coffee"]:
                 return True
+            else:
+                return False
         else:
             if res["water"] > ingredients["water"] and res["milk"] > ingredients["milk"] and res["coffee"] > ingredients["coffee"]:
                 return True
+            else:
+                return False
 
 
 coffee = CoffeeMachine()
